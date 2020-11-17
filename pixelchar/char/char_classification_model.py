4f1f273beabff2393edc0f5ce0c2fa15ac4d770f
yanyu <godpgf@qq.com>
2020-11-17 17:54:59 +0800

    continue

--- pixelchar/char/char_classification_model.py
+++ pixelchar/char/char_classification_model.py
@@ -59,7 +59,7 @@ class CharClassificationModel(CharModel):
                         except StopIteration as e:
                             break
 
-                    if eval_data_source_factory is not None and p_label_name is not None:
+                    if eval_data_source_factory is not None:
                         eval_data_iter = iter(eval_data_source)
                         while True:
                             try:
@@ -67,8 +67,14 @@ class CharClassificationModel(CharModel):
                                                                              attach_data_size)
                                 feed_dict[self.db["epoch_idx"]] = [epoch_index]
                                 label = feed_dict[self.db[label_name]]
-                                loss, predict = self.sess.run([self.db[eval_loss_name], self.db[p_label_name]],
-                                                              feed_dict=feed_dict)
+                                if p_label_name is not None:
+                                    loss, predict = self.sess.run([self.db[eval_loss_name], self.db[p_label_name]],
+                                                                  feed_dict=feed_dict)
+                                else:
+                                    loss = self.sess.run(
+                                        [self.db[train_loss_name]],
+                                        feed_dict=feed_dict)
+                                    predict = None
                                 if char_eval_list is not None:
                                     for char_eval in char_eval_list:
                                         char_eval.push(loss, predict, label, attach_data_list)
